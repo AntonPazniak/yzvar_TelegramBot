553f289d6b51fa808aec286d15c072f4e1449e61
AntonPazniak <poznyak969@gmail.com>
2025-04-12 14:09:23 +0200

    initial

---
+++ src/main/java/com/example/yzvar_telegrambot/YzvarTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.yzvar_telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YzvarTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YzvarTelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yzvar_telegrambot/YzvarTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yzvar_telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YzvarTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
